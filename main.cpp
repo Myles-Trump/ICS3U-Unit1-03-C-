28922d4ae4d9b09bda5c3c7ab1d7e54bd5704225
MylesTrump <>
2021-05-24 00:38:46 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2020 Myles Trump All rights reserved
+//
+// Created by: Myles Trump
+// Created on: May 2021
+// This is the "Hello, World!" program, with proper style
+
+#include <iostream>
+  
+int main() {
+  std::cout << "Hello, World!";
+}
